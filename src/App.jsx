052cd3767e9e9f08de0a8708902b4b015868a285
BenVagan <bdecarvalho.42@gmail.com>
2024-06-05 01:18:39 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import ToolBar from "./components/ToolBar";
+
+function App() {
+  return (
+    <div class="container m-auto">
+      <header>
+          <h1>Moonpedia</h1>
+          <ToolBar />
+      </header>
+          <p>
+            Welcome to Moonpedia,
+            the free encyclopedia by the Moon People.
+            999.999.999 articles in English
+          </p>
+          <h2>
+            Featured Article
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil ea debitis nulla! A repudiandae eius sunt nulla velit, ex aut voluptates provident, quia reprehenderit eveniet. Repudiandae inventore quaerat ipsam quae molestiae totam veritatis dolorum. Aspernatur culpa recusandae, totam quasi vitae eveniet deserunt!
+          </p>
+          <h2>
+            Random Facts
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse facere molestias dignissimos quisquam, tempora deleniti rerum modi laborum quos quibusdam soluta recusandae, a porro officiis consequuntur! Deserunt reprehenderit excepturi ipsum enim asperiores.  
+          </p>
+          <h2>
+            Hot Topic
+          </h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error, provident tenetur alias, natus maiores ut ad consequatur impedit, necessitatibus aspernatur explicabo sit debitis vero? Consectetur architecto, consequatur, aspernatur sint ratione quibusdam atque reiciendis temporibus dicta veritatis tenetur. 
+          </p>
+          <h2>
+            Today in History
+          </h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est libero, velit quos blanditiis modi enim tenetur atque distinctio, reiciendis aut quae, dignissimos facere fuga temporibus praesentium esse.
+          </p>
+    </div>
+  );
+}
+
+export default App;
